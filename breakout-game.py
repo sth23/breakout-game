a0b17c09fbfdc4b4749ea6331e4b2dbb83f0df86
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-08 19:58:46 -0400

    Updated from Brython Server: 5/8/2019 7:58:48 PM

--- breakout-game.py
+++ breakout-game.py
@@ -109,6 +109,8 @@ class BreakoutGame(App):
                 self.ball.vy = self.ball.vy * math.sin(math.pi/2)
                 self.ball.vx = -self.ball.vy
             elif self.ball.x > self.player.x - self.player.width * 3 / 4:
+                self.ball.vy = self.ball.vy * math.sin(math.pi/2)
+                self.ball.vx = self.ball.vy
         
         # Ball falling down past paddle
         if self.ball.y > self.height and self.lives > 0:
