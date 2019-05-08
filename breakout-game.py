642b355c0724cdad43233546e8d0675eebd6aa46
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-08 13:23:43 -0400

    Updated from Brython Server: 5/8/2019 1:23:44 PM

--- breakout-game.py
+++ breakout-game.py
@@ -1,1 +1,10 @@
+"""
+Final Project: Breakout Game
+Author: Sean
+Credit: Tutorials
+Assignment: Create an old-school Breakout game
+"""
 
+from ggame import App, RectangleAsset, CircleAsset, Sprite, LineStyle, Color
+import math
+import random
