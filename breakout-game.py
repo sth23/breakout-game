741b411fe4af06cfd3f186e71b9661ecf83681f2
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-08 19:44:54 -0400

    Updated from Brython Server: 5/8/2019 7:44:55 PM

--- breakout-game.py
+++ breakout-game.py
@@ -98,9 +98,9 @@ class BreakoutGame(App):
         self.ball.step()
         
         # Ball bouncing off walls
-        if self.ball.x == 10:
+        if self.ball.x > 10:
             self.ball.vx = -self.ball.vx
-        elif self.ball.x == self.width - 10:
+        elif self.ball.x > self.width - 10:
             self.ball.vx = -self.ball.vx
             
         # Ball bouncing off ceiling
@@ -117,8 +117,6 @@ class BreakoutGame(App):
             print("Game Over")
             self.ball.destroy()
             self.destroy()
-            
-        print(self.ball.x)
         
 myapp = BreakoutGame()
 myapp.run()
