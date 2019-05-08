c0f1f25d45c2579ffa9c00f524a41b9bc84dc146
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-08 15:03:09 -0400

    Updated from Brython Server: 5/8/2019 3:03:10 PM

--- breakout-game.py
+++ breakout-game.py
@@ -20,7 +20,7 @@ class Ball(Sprite):
     
     def __init__(self, position):
         super().__init__(Ball.circ, position)
-        self.speed = 10
+        self.speed = 2
         self.vx = 0
         self.vy = self.speed
         
