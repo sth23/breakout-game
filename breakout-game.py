a81d4fdf437fe28b9883c7d4ce46d3ab53a3cc94
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-08 20:13:15 -0400

    Updated from Brython Server: 5/8/2019 8:13:16 PM

--- breakout-game.py
+++ breakout-game.py
@@ -91,10 +91,10 @@ class BreakoutGame(App):
         
         # Create bricks
         self.brickwidth = (self.width - self.wall.width * 2 - 11 * 10) / 10
-        self.brickasset = RectangleAsset(self.brickwidth, 10, BreakoutGame.noline, BreakoutGame.black)
+        self.brickasset = RectangleAsset(self.brickwidth, 20, BreakoutGame.noline, BreakoutGame.black)
         for row in range(0,6):
             for column in range(0,10):
-                Bricks(self.brickasset, (10 + column * 120, row * 20 + 150))
+                Bricks(self.brickasset, (20 + column * 120, row * 20 + 150))
         
         # Create player & ball
         self.player = Paddle((self.width/2, self.height - 50), self.width)
